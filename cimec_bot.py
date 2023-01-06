6042abf1d9000c40fd952188eeb8c91eba141453
miltend <miltendean@gmail.com>
2023-01-06 17:18:12 +0100

    test

--- cimec_bot.py
+++ cimec_bot.py
@@ -29,9 +29,6 @@ def start_message(message):
 def news_message(message):
     news_html = soup.find(text="News").parent.next_sibling.next_sibling
     news_list = handle_posts(news_html)
-    # news = news_html.find_all(attrs="view-content")[0].find_all("a")
-    # for new in news:
-    #     news_list.append(new.text.strip() + f"\n<i>Read more:\t <a href='{new['href']}'>link</a></i>" + "\n\n")
     bot.send_message(message.chat.id, "".join(news_list), parse_mode="HTML")
 
 
@@ -39,9 +36,6 @@ def news_message(message):
 def events_message(message):
     event_html = soup.find("h2", text="Events").parent
     events_list = handle_posts(event_html)
-    # events = event_html.find_all(attrs="view-content")[0].find_all("a")
-    # for event in events:
-    #     events_list.append(event.text.strip() + f"\n<i>Read more:\t <a href='{event['href']}'>link</a></i>" + "\n\n")
     bot.send_message(message.chat.id, "".join(events_list), parse_mode="HTML")
 
 
@@ -55,5 +49,6 @@ bot.infinity_polling()
 
 if __name__ == '__main__':
     app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
